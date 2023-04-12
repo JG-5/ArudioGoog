a50cbf9ccc5d3f4d8502af4fd16ab40e45519d68
JG-5 <S1670428@online.houstonisd.org>
2023-04-12 10:01:50 -0500

    init

---
+++ googlesheetsscript.py
@@ -0,0 +1,41 @@
+import serial
+import time
+import datetime
+from google.oauth2 import service_account
+from googleapiclient.discovery import build
+
+# Set up Google Sheets API credentials
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+SERVICE_ACCOUNT_FILE = 'creds.json'
+creds = None
+creds = service_account.Credentials.from_service_account_file(
+    SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+
+# Set up serial connection to Arduino sensor
+ser = serial.Serial('/dev/ttyACM0', 9600)
+
+# Set up Google Sheets API client
+sheet_id = 'AIzaSyBNwPTSXkSVZDdHmwE_QHSTJg7sqXU53Mk'
+sheet_range = 'Sheet1!A:B'
+service = build('sheets', 'v4', credentials=creds)
+sheet = service.spreadsheets()
+
+# Loop to read data from sensor and write it to the sheet
+while True:
+    # Read data from sensor
+    data = ser.readline().decode().strip().split(',')
+
+    # Format data for Google Sheets API
+    values = [
+        [datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')] + data
+    ]
+
+    # Write data to sheet
+    request_body = {
+        'values': values
+    }
+    sheet.values().append(spreadsheetId=sheet_id, range=sheet_range,
+                           valueInputOption='USER_ENTERED', body=request_body).execute()
+
+    # Wait for some time before reading again
+    time.sleep(5)
